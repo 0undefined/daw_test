ccc93530fe9f1578e2bf98071631db3459ba6bb0
0undefined <oscar@nelin.dk>
2025-03-27 16:11:20 +0100

    Add some worldgen

---
+++ include/worldgen.h
@@ -0,0 +1,24 @@
+#ifndef STATES_WORLDGEN_H
+#define STATES_WORLDGEN_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <engine/core/types.h>
+
+// first 4 bits for material type
+#define BLOCK_none    0
+#define BLOCK_grass   1
+#define BLOCK_rock    2
+
+// next 4 bits for shape type
+#define SHAPE_filled (1 << 4)
+#define SHAPE_slope  (2 << 4)
+
+void gen_terrain(u8 *world, usize height, usize length, usize width);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
--- state_mainstate/include/states/mainstate.h
+++ state_mainstate/include/states/mainstate.h
@@ -13,11 +13,17 @@
 
 #include <cglm/cglm.h>
 
+#define WORLD_HEIGHT 32
+#define WORLD_WIDTH  32
+#define WORLD_LENGTH 32
+#define WORLD_SIZE   (WORLD_HEIGHT * WORLD_LENGTH * WORLD_WIDTH)
+
 typedef struct mainstate_state {
   /* Resources */
   Shader       shaders[10];
   RenderBatch  terrain;
   RenderObject objects[10];
+  u8 world[WORLD_HEIGHT * WORLD_WIDTH * WORLD_LENGTH];
 	i_ctx input_ctx;
   binding_t input_bindings[10];
   vec3 cam_dir;
